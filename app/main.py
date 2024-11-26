d093506c6a0001cd5a6fe1a6b02121501431536e
Wojciech Piotr Gula <gulasz101@gmail.com>
2024-11-26 16:48:16 +0100

    Returning url argument passed as /foo/{bar}

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket  # noqa: F401
+import re
 
 
 def main():
@@ -10,11 +11,20 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, _addr = server_socket.accept()  # wait for client
 
-    match conn.recv(1024).decode("utf-8").split(" ")[1]:
-        case "/":
-            response = b"HTTP/1.1 200 OK\r\n\r\n"
-        case _:
-            response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    request_uri = conn.recv(1024).decode("utf-8").split(" ")[1]
+    if re.match("^/([a-zA-Z])*/([a-zA-Z])*", request_uri):
+        response_body = request_uri.split("/")[1]
+        response = "\r\n".join(
+            {
+                "HTTP/1.1 200 OK",
+                "Content-Type: text/plain",
+                "Content-Length: " + str(response_body.__len__()),
+                response_body,
+            }
+        )
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
     conn.send(response)
 
