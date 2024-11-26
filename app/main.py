8fd5353860241ae941ecfa4e71d3d42cda1b77c7
Wojciech Piotr Gula <gulasz101@gmail.com>
2024-11-26 23:03:43 +0100

    Handling file stream.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import socket  # noqa: F401
 import re
 from threading import Thread
+import sys
+import io
 
 
 class REqual(str):
@@ -10,12 +12,31 @@ class REqual(str):
         return bool(re.fullmatch(pattern, self))
 
 
-def handle_connection(conn):
+def handle_connection(conn, directory=None):
     request = conn.recv(1024).decode("utf-8")
     request_uri = request.split(" ")[1]
     match REqual(request_uri):
         case r"^/$":
             response = b"HTTP/1.1 200 OK\r\n\r\n"
+        case r"^/files/([a-zA-Z])*":
+            file_name = request_uri.split("/")[2]
+
+            try:
+                file_handler = io.open(str(directory) + "/" + file_name)
+                file_contents = file_handler.read()
+
+                response = "\r\n".join(
+                    [
+                        "HTTP/1.1 200 OK",
+                        "Content-Type: application/octet-stream\r\nContent-Length: "
+                        + str(file_contents.__len__())
+                        + "\r\n",
+                        file_contents,
+                    ]
+                ).encode("utf-8")
+
+            except IOError:
+                response = b"HTTP/1.1 404 Not Found\r\n\r\n"
         case r"^/user-agent$":
             request_pieces = request.split("\r\n")
             # unset unwanted parts
@@ -49,19 +70,40 @@ def handle_connection(conn):
             response = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
     conn.send(response)
+    conn.close()
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
+    directory = None
+    try:
+        arg = sys.argv[1].strip("-").split("=")
+        arg_name = arg[0]
+        arg_value = arg[1]
 
-    # Uncomment this to pass the first stage
-    #
+        print(
+            (
+                arg_name,
+                arg_value,
+            )
+        )
+
+        if arg_name != "directory":
+            raise NameError("Unknown argument")
+        directory = arg_value
+    except IndexError:
+        pass
+
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     while True:
         conn, _addr = server_socket.accept()  # wait for client
-        t = Thread(target=handle_connection, args=(conn,))
+        t = Thread(
+            target=handle_connection,
+            args=(
+                conn,
+                directory,
+            ),
+        )
         t.start()
 
 
